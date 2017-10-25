d55f67254384221dc3879784aecdcff77729ca14
Alaster94 <aragonnehemias@gmail.com>
2017-10-25 00:22:23 -0600

    Tarea POO

---
+++ Tarea#1/src/sample/Persona.java
@@ -0,0 +1,36 @@
+package sample;
+
+public class Persona {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    private static Persona[] listaPersonas = {
+            new Persona("David","Delcid",21),
+            new Persona("Alaster","Erazo",24),
+            new Persona("Juan","Velis",22),
+            new Persona("Josue","Cordoba",20),
+            new Persona("Yarince","Padilla",27),
+            new Persona("Oscar","Diaz",25),
+            new Persona("Orlando","Calix",22),
+
+    };
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public static Persona buscarPorPosicion(String nombre) {
+        int i = 0;
+        while (i < listaPersonas.length){
+            if (listaPersonas[i].nombre.equals(nombre)) {
+                return listaPersonas[i];
+            }
+            i++;
+        }
+        return null;
+    }
+
+
+}
